5461a2b7bf716fd248078795dede050e27c8da71
Alan de Sousa Carvalho <alan.sousacarvalho@outlook.com>
2025-07-01 11:26:07 -0300

    JPA repository, injeção de dependencia e database seeding

---
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.educandoweb.curso.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.repositories.UserRepository;
+
+@Configuration 
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Alan", "alan@gmail.com", "999", "123456");
+		userRepository.save(u1);
+	}
+}
