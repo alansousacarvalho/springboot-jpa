9a9ca1fb839d4fe465f61e1fd4a0838fbfcb5846
Alan <alan.sc2015@hotmail.com>
2025-07-25 17:29:15 -0300

    Finalização da criação do OrderItem com a associação com os dados extras

--- src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.curso.entities.Category;
 import com.educandoweb.curso.entities.Order;
+import com.educandoweb.curso.entities.OrderItem;
 import com.educandoweb.curso.entities.Product;
 import com.educandoweb.curso.entities.User;
 import com.educandoweb.curso.entities.enums.OrderStatus;
 import com.educandoweb.curso.repositories.CategoryRepository;
+import com.educandoweb.curso.repositories.OrderItemRepository;
 import com.educandoweb.curso.repositories.OrderRepository;
 import com.educandoweb.curso.repositories.ProductRepository;
 import com.educandoweb.curso.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner { // Popular o BD.
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception { // Método do "CommandLinerRunner"; Inicia na execução 
@@ -67,5 +72,12 @@ public class TestConfig implements CommandLineRunner { // Popular o BD.
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u1);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
