47d5c9953d319ec72a395863d70063672ea76774
Alan <alan.sc2015@hotmail.com>
2025-07-29 18:00:49 -0300

    Tratamento exceção parcial

---
+++ src/main/java/com/educandoweb/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.educandoweb.curso.resources.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	
+}
