9b70a44765e9fe961f8faf4080f8dad64105535f
Alan <alan.sc2015@hotmail.com>
2025-07-31 08:47:44 -0300

    Inclusão de tratamento de exceção ao tentar resgatar um User inexistente

--- src/main/java/com/educandoweb/curso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -1,9 +1,24 @@
 package com.educandoweb.curso.resources.exceptions;
 
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.educandoweb.curso.services.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException err, HttpServletRequest request) {
+		String message = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError(Instant.now(), status.value(), err.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
 }
--- src/main/java/com/educandoweb/curso/services/UserService.java
+++ src/main/java/com/educandoweb/curso/services/UserService.java
@@ -21,7 +21,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
